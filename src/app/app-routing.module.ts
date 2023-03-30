d550f4123260c253e12bcff34799b89340afb6d8
Venceslas Kassi Kacou <40325403+kasperobig@users.noreply.github.com>
2023-03-30 04:46:29 +0200

    Creation des components home career, creation des repertoires services et directive puis les branchements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CareerComponent } from './career/career.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'career', component: CareerComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
